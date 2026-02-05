53b9c2e14441a75d6c201c32211b7f49db85b92d
Mahesh Rao <maheshrao@Maheshs-MacBook-Air.local>
2026-02-05 21:50:57 +0530

    Fix: Align DATABASE_HOST default with docker-compose

--- my-strapi-project/config/database.js
+++ my-strapi-project/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', 'strapi_postgres'),
+            host: env('DATABASE_HOST', 'postgres'),
             port: env.int('DATABASE_PORT', 5432),
             database: env('DATABASE_NAME', 'strapi'),
             user: env('DATABASE_USERNAME', 'strapi'),
